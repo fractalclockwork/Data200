f8dafecec6da87b0ce99573ea4c711d03f4b59d3
Brent A. Thorne <brentathorne@gmail.com>
2024-03-23 11:33:39 -0700

    minor fix for data download

--- Utils/get_data.sh
+++ Utils/get_data.sh
@@ -1,1 +1,1 @@
-conda run --no-capture-output -n data200s gdown https://drive.google.com/uc?id=1-be6hZE61fYibBQ23VUUAg8oE8AfpPq1 -cO 'Data/sp24_grad_project_data.zip'
+conda run --no-capture-output -n data200s gdown https://drive.google.com/uc?id=1-be6hZE61fYibBQ23VUUAg8oE8AfpPq1 -O 'Data/sp24_grad_project_data.zip'
