01f203f218cd5962797f6c860dc065a35195f76c
Brent A. Thorne <brentathorne@gmail.com>
2024-03-24 09:43:20 -0700

    more diagrams and testing of env setup

--- Utils/run_conda.sh
+++ Utils/run_conda.sh
@@ -1,1 +1,2 @@
-conda run --no-capture-output -n data200s jupyter lab Source/Checkpoint_1_EDA.ipynb
+#conda run --no-capture-output -n data200s jupyter lab Source/Checkpoint_1_EDA.ipynb
+conda run -n data200s jupyter lab Source/Checkpoint_1_EDA.ipynb
