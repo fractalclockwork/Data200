a21cbff407d0cb6ef342a6d7b94b1dcd4a7ec770
Brent A. Thorne <brentathorne@gmail.com>
2024-07-09 09:07:20 -0700

    updated for current tensorflow-gpu version

--- FinalPoject/utils/run_conda_model.sh
+++
@@ -1,2 +0,0 @@
-#conda run --no-capture-output -n data200s jupyter lab Source/Checkpoint_2_Modeling.ipynb
-conda run -n data200s jupyter lab analysis/Final_Report.ipynb
---
+++ FinalProject/utils/run_conda_model.sh
@@ -0,0 +1,4 @@
+#conda run --no-capture-output -n data200s jupyter lab Source/Checkpoint_2_Modeling.ipynb
+#conda run -n data200s jupyter lab analysis/Final_Report.ipynb conda run -n data200s jupyter lab analysis/Final_Project_Notebook.ipynb
+conda run -n data200s jupyter lab 
+#conda run -n data200s pwd 
